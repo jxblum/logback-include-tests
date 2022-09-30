1ff81c14d6eafc743690c4d95381ac5850ad3120
John Blum <jblum@vmware.com>
2022-09-30 12:47:20 -0700

    Edit copyright and format source code.

--- src/test/java/example/test/logback/LogbackLoggingIntegrationTests.java
+++ src/test/java/example/test/logback/LogbackLoggingIntegrationTests.java
@@ -13,7 +13,6 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 package example.test.logback;
 
 import static org.assertj.core.api.Assertions.assertThat;
